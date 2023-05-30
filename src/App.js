94a5f7f7d47e12690ffe0f7d8f0ef7c2c77c915b
ArthurRamon9 <84184700+ArthurRamon9@users.noreply.github.com>
2023-05-30 12:06:53 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import Header from   './components/Header/Header';
+import Main from './components/Main/Main';
+import Footer from './components/Footer/Footer'
+
+
+function reproduzVideo() {
+  alert("O vídeo está sendo reproduzido")
+}
+
+function App() {
+  return(
+
+      <>
+        <Header/>
+        <Main/>
+        <Footer/>
+      
+      </>
+  )
+}
+
+export default App;
---
+++ src/components/Main/Main.js
@@ -0,0 +1,13 @@
+import Menu from './Menu/Menu.js'
+import Cards from './Cards/Cards.js'
+
+function Main(){
+    return(
+    <>
+    <Menu/>
+    <Cards/>
+    </>
+    )
+}
+
+export default Main
